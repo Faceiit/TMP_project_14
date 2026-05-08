291f9526afa87d4547125bc0bd18a10da12ea856
MrIon4ik <ageevoleg228@gmail.com>
2026-05-08 20:04:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include <QCoreApplication>
-#include "database.h"
+#include <QApplication>
 #include "mytcpserver.h"
+#include "loginwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    Database::getInstance();  // инициализация БД
+    QApplication a(argc, argv);
+
+    // Запуск сервера
     MyTcpServer server;
+
+    // Запуск клиента GUI
+    LoginWindow w;
+    w.show();
+
     return a.exec();
 }
